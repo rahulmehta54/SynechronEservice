2c589204758aaed19ee197b365f133ec350f8bc9
jatkardigvijay <jatkardigvijay@gmail.com>
2020-02-10 15:25:19 +0530

    project inti

---
+++ src/main/java/com/example/entity/Admin.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Admin {
+
+	private String username;
+	private String password;
+}
