92ae982d39ccc3413f6f1a9248d801fa9539547c
vermabhupendra <bhupendraverma.java@gmail.com>
2020-02-11 11:49:05 +0530

    completed ctl,dto,dao,jsp's etc

--- src/main/java/com/example/demo/daoimpl/UserDaoImpl.java
+++ src/main/java/com/example/demo/daoimpl/UserDaoImpl.java
@@ -1,8 +1,5 @@
 package com.example.demo.daoimpl;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -18,12 +15,29 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public int loginUser(User user) {
-		int i = 0;
-		String sql = "select * from tbl_user where email= ?  and password=?";
-		List<Map<String, Object>> list = this.template.queryForList(sql, user.getEmail(), user.getPassword());
-		if (list != null && list.size() == 1) {
+		//0 - Not found
+		//1 - admin
+		//2 - user
 		
-		} 
+		int i = 0;
+		String sql = "select count(*) from tbl_user where email=? and password=?";
+		/*
+		 * List<Map<String, Object>> list = this.template.queryForList(sql,
+		 * user.getEmail(), user.getPassword()); if (list != null && list.size() == 1) {
+		 * 
+		 * }
+		 */
+
+		int val = this.template.queryForObject(sql, new Object[] { user.getEmail(), user.getPassword() },
+				Integer.class);
+
+		if (val == 1) {
+			String sqlQuery = "select userType from tbl_user where email=? and password=?";
+		}else {
+			
+		}
+
+		System.out.println("==========" + val + "===========");
 		return i;
 	}
 }
